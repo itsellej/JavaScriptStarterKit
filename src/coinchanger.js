d9a5bd115dfcde51ad1a0ffb3a5ade40b7ccb84d
itsellej <daniellehallal@gmail.com>
2018-12-14 10:08:43 +0000

    Test passing: returns not a coin if money is not equal to coin or 3. Refactor coinchanger to use a for loop

--- src/coinchanger.js
+++ src/coinchanger.js
@@ -1,10 +1,16 @@
 const changeCoins = (money) => {
+  const coins = [200, 100, 50, 20, 10, 5, 2, 1];
+
+  for(let i = 0; i < coins.length; i++) {
+    if(coins[i] === money){
+      return [money];
+    }
+  }
   if(money === 3){
     return [2, 1];
   } else {
-    return [money];
+    return 'not a coin';
   }
-
 }
 
 module.exports = { changeCoins };
