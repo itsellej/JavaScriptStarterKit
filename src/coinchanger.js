bdffe623c81f310f65e83d66edbcb21ae6ce2d5f
itsellej <daniellehallal@gmail.com>
2018-12-14 10:47:02 +0000

    Test passing: returns [2, 2] when given 4. Test returns not a coin no longer required

--- src/coinchanger.js
+++ src/coinchanger.js
@@ -1,16 +1,14 @@
 const changeCoins = (money) => {
   const coins = [200, 100, 50, 20, 10, 5, 2, 1];
+  const giveChange = []
 
-  for(let i = 0; i < coins.length; i++) {
-    if(coins[i] === money){
-      return [money];
+  coins.forEach(coin => {
+    while(money >= coin){
+      giveChange.push(coin);
+      money -= coin;
     }
-  }
-  if(money === 3){
-    return [2, 1];
-  } else {
-    return 'not a coin';
-  }
+  })
+  return giveChange;
 }
 
 module.exports = { changeCoins };
